62b50a502e0fb8cae6f1edb24b2ac00c9fc95ad4
j.samcovic <jsamcovic1996@gmail.com>
2026-04-25 04:00:03 +0200

    fix error with deploy

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default ({ env }) => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME", "placeholder"),
+        api_key: env("CLOUDINARY_KEY", "placeholder"),
+        api_secret: env("CLOUDINARY_SECRET", "placeholder"),
+      },
+    },
+  },
+});
